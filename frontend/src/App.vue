2bff55d8622d85d4168a40d088862d45132fb523
200713-saito-kengo <k_saito@freestyles.jp>
2021-11-17 20:55:50 +0900

    [斉藤 2011/11/17] @0.25H プロジェクト作成（完了） @0.75H ヘッダーコンポーネント作成（作業中）

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<Header></Header>
+
+</template>
+
+<script>
+import Header from './components/Header.vue'
+// import SearchArea from './components/SearchArea.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+<header>
+    <h1>{{ title }}</h1>
+    <div id="subtitle">{{ subtitle }}</div>
+    <div id="search">
+        <router-link to="/">{{ search }}</router-link>
+    </div>
+    <div id="register">
+        <router-link to="/new">{{ register }}</router-link>
+    </div>
+</header>
+</template>
+
+<script>
+export default {
+    name: 'header',
+    data() {
+        return {
+            title: "販売管理システム",
+            subtitle: "SALESWORKERS",
+            search: "見積検索",
+            register: "見積登録"
+        }
+    }
+}
+</script>
+
+<style>
+    header {
+        background-color: #000;
+        color: #fff;
+        height: 50px;
+        display: grid;
+        grid-template-columns: 20px 1fr 20px;
+        grid-template-rows: 1fr;
+        margin-bottom: 80px;
+    }
+
+    header > * {
+        grid-row: 1;
+        grid-column: 2;
+    }
+
+    h1 {
+        justify-self: start;
+        align-self: center;
+        font-size: 20px;
+        font-weight: normal;
+    }
+    
+    #subtitle {
+        justify-self: end;
+        align-self: center;
+        display: none;
+    }
+
+</style>
